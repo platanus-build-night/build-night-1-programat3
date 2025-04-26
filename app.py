c0c7246a0c1a5f9e8204da4904ad03ac49e36190
liadasilva <liadasilva@skalling.com>
2025-04-25 20:19:21 -0400

    cambios y formulario listos

--- app.py
+++ app.py
@@ -14,20 +14,15 @@ app.config.from_file("config.json", load= json.load)
 GoogleMaps(app, key=app.config["GOOGLEMAPS_KEY"])
 
 class GeolocForm(Form):
-    address = StringField("Address", [validators.Length(min=1, max=200)])
+    adress = StringField("Address", [validators.Length(min=1, max=200)])
     submit = BooleanField("Submit")
-    def validate(self):
-        if not super().validate():
-            return False
-        if not self.address.data:
-            self.address.errors.append("Address is required.")
-            return False
-        return True
+
+
 @app.route("/", methods=["GET", "POST"])
 def geoloc():
     form = GeolocForm(request.form)
     if request.method == "POST" and form.validate():
-        address = form.address.data
-        coordinates = get_coordinates(address)
-        return render_template("geoloc.html", form=form, coordinates=coordinates)
+        adress = form.adress.data
+        coordinates = get_coordinates(address_text=adress, API_KEY=app.config["GOOGLEMAPS_KEY"])
+        return render_template("coordinates.html",coordinates=coordinates)
     return render_template("index.html", form=form)
---
+++ functions_varias.py
@@ -0,0 +1,2 @@
+def get_zone(latitude, longitude):
+   pass
