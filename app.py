34ddc74ad85f5ac5840ac71641dac02b0440dfdf
liadasilva <liadasilva@skalling.com>
2025-04-25 21:43:19 -0400

    Data coordenadas y algunas funciones

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from flask_googlemaps import get_coordinates, GoogleMaps
 
 from wtforms import Form, BooleanField, StringField, validators
 
-from functions_varias import get_zone, 
+from functions_varias import get_zone, generate_prospect_tinder_profile, get_prospects, get_prospect_details
+
 app = Flask(__name__)
 
 app.config.from_file("config.json", load= json.load)
@@ -25,6 +26,11 @@ def geoloc():
     if request.method == "POST" and form.validate():
         adress = form.adress.data
         coordinates = get_coordinates(address_text=adress, API_KEY=app.config["GOOGLEMAPS_KEY"])
-        
-        return render_template("coordinates.html",coordinates=coordinates)
+        prospects = get_prospects(get_zone(coordinates["lat"], coordinates["lng"]))
+        dict_prospect_profiles = {}
+        for p in prospects:
+            prospect_details = get_prospect_details(p)
+            tinder_p = generate_prospect_tinder_profile(prospect_details, app.config["GEMINI_KEY"])
+            dict_prospect_profiles[p] = tinder_p
+        return render_template("coordinates.html",dict_prospects=dict_prospect_profiles)
     return render_template("index.html", form=form)
--- functions_varias.py
+++ functions_varias.py
@@ -1,8 +1,11 @@
 import json
 import pandas as pd
 import os
+import google.generativeai as genai
+
 def get_zone(latitude, longitude):
    pass
+
 def get_prospects(zone, nombre):
     with open("/data/especies_zona.json", "r") as file:
         data = json.load(file)
@@ -23,4 +26,20 @@ def get_prospect_details(prospect):
     else:
         df = df.to_dict()
         return df
-def generate_prospect_tinder_profile(prospect_dict):
+def generate_prospect_tinder_profile(prospect_dict, apiKey):
+    genai.configure(api_key=apiKey)
+    prompt = f"Genera un perfil similar a Tinder para la especie {prospect_dict['accepted_species']} con las siguientes características: {prospect_dict} redactadas de forma divertida y en español"
+    generation_config = {
+    "temperature": 0,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+    }
+    model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    )
+    response = model.generate(prompt)
+    response = response.text
+    return response
